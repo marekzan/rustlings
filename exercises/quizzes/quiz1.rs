eedb1c40ebe0aa0ea2f66c4e671d3780653ff0ed
marekzan <marekzan25@gmail.com>
2024-10-14 21:14:20 +0200

    until move semantics 1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(num_of_apples: i32) -> i32 {
+    if num_of_apples > 40 {
+        num_of_apples
+    } else {
+        num_of_apples * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
